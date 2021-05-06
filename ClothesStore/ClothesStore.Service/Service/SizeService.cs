ae1897092a4f3f9e51c253699204e9f6189e00a1
khuatchung1012 <khuatchung1012@gmail.com>
2021-05-06 20:58:38 +0700

    done

---
+++ ClothesStore/ClothesStore.Service/Service/SizeService.cs
@@ -0,0 +1,108 @@
+﻿using ClothesStore.Model.Model.EF;
+using ClothesStore.Model.ModelView;
+using ClothesStore.Service.IService;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClothesStore.Service.Service
+{
+    public class SizeService : ISizeService
+    {
+        ClothingStoreContext db = new ClothingStoreContext();
+        public async Task<bool> AddOrUpdate(Size size)
+        {
+            try
+            {
+                if (size.Id == 0)
+                {
+                    size.CreatedDate = DateTime.Now;
+                    size.IsDeleted = false;
+                    await db.Sizes.AddAsync(size);
+                    await db.SaveChangesAsync();
+                    return true;
+                }
+                else
+                {
+                    var pro = await db.Sizes.FindAsync(size.Id);
+                    pro.Name = size.Name;
+                    pro.OrderBy = size.OrderBy;
+                    pro.UpdatedDate = size.UpdatedDate;
+                    pro.Status = size.Status;
+                    db.SaveChanges();
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
+        public async Task<bool> DeleteById(int Id)
+        {
+            var obj = await db.Sizes.FindAsync(Id);
+            if (obj != null)
+            {
+                obj.IsDeleted = true;
+                await db.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<List<Size>> GetAll()
+        {
+            return await db.Sizes.Where(x => x.IsDeleted == false).ToListAsync();
+        }
+
+        public async Task<ResponseData<Size>> GetListData(RequestData requestData)
+        {
+            var data = await db.Sizes.Where(x => x.IsDeleted == false).ToListAsync();
+            // get total records
+            var totalRecords = data.Count();
+            // filter
+            if (requestData.ListFilter != null)
+            {
+                foreach (var filter in requestData.ListFilter)
+                {
+                    data = data.Where(x => x.GetType().GetProperty(filter.Key).PropertyType.Name == "String" ? x.GetType().GetProperty(filter.Key).GetValue(x).ToString().Contains(filter.Value) : x.GetType().GetProperty(filter.Key).GetValue(x).Equals(filter.Value)).ToList();
+                }
+                totalRecords = data.Count();
+            }
+
+            // sort by
+            if (!String.IsNullOrEmpty(requestData.OrderBy))
+            {
+                if (requestData.IsAsc)
+                    data = data.OrderBy(x => x.GetType().GetProperty(requestData.OrderBy).GetValue(x)).ToList();
+                else
+                    data = data = data.OrderByDescending(x => x.GetType().GetProperty(requestData.OrderBy).GetValue(x)).ToList();
+            }
+            //pagination
+            if (requestData.PageNumber != 0)
+            {
+                data = data.Skip(requestData.PageSize * (requestData.PageNumber - 1)).Take(requestData.PageSize).ToList();
+            }
+
+            ResponseData<Size> responseData = new ResponseData<Size>()
+            {
+                Data = data,
+                PageCount = totalRecords % requestData.PageSize == 0 ? totalRecords / requestData.PageSize : totalRecords / requestData.PageSize + 1,
+                PageNumber = requestData.PageNumber,
+                PageSize = requestData.PageSize,
+                OrderBy = requestData.OrderBy,
+                IsAsc = requestData.IsAsc
+            };
+            return responseData;
+        }
+
+        public async Task<Size> GetObjectById(int Id)
+        {
+            return await db.Sizes.FindAsync(Id);
+        }
+    }
+}
---
+++ ClothesStore/ClothesStore.WebApp/Areas/Admin/Controllers/SizeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClothesStore.Model.Model.EF;
+using ClothesStore.Model.ModelView;
+using ClothesStore.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClothesStore.WebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SizeController : Controller
+    {
+        ISizeService _sizeService;
+
+        public SizeController(ISizeService sizeService)
+        {
+            _sizeService = sizeService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<PartialViewResult> GetFormAddOrEdit(int Id)
+        {
+            if (Id == 0)
+                return PartialView(null);
+            else
+                return PartialView(await _sizeService.GetObjectById(Id));
+        }
+
+        [HttpPost]
+        public async Task<PartialViewResult> ViewDetail(int Id)
+        {
+            var obj = await _sizeService.GetObjectById(Id);
+            var data = new ViewDetailObject<Size>()
+            {
+                obj = obj,
+                CreatedBy = "",
+                UpdatedBy = ""
+            };
+            return PartialView(data);
+        }
+
+        [HttpPost]
+        public async Task<PartialViewResult> GetListData(RequestData requestData)
+        {
+            var response = await _sizeService.GetListData(requestData);
+            return PartialView(response);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> GetObjectById(int Id)
+        {
+            var response = await _sizeService.GetObjectById(Id);
+            return Json(response);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> AddOrUpdate(Size obj)
+        {
+            var response = await _sizeService.AddOrUpdate(obj);
+            return Json(new ResponseStatus() { success = response, error = null });
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> DeleteById(int Id)
+        {
+            var response = await _sizeService.DeleteById(Id);
+            return Json(response);
+        }
+    }
+}
